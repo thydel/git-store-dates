092fcd2879da5cbd79ff66ea6bc0001650570ca4
Thierry Delamare <t.delamare@laposte.net>
2020-04-20 21:08:49 +0200

    Updates boilerplate

--- git-store-dates.mk
+++ git-store-dates.mk
@@ -1,10 +1,38 @@
 #!/usr/bin/make -f
 
 MAKEFLAGS += -Rr
+MAKEFLAGS += --warn-undefined-variables
 SHELL := $(shell which bash)
+.SHELLFLAGS := -euo pipefail -c
+
+.ONESHELL:
+.DELETE_ON_ERROR:
+.PHONY: phony
+
 .RECIPEPREFIX :=
 .RECIPEPREFIX +=
-.ONESHELL:
+
+.DEFAULT_GOAL := main
+
+MIN_VERSION := 4.1
+VERSION_ERROR :=  make $(MAKE_VERSION) < $(MIN_VERSION)
+$(and $(or $(filter $(MIN_VERSION),$(firstword $(sort $(MAKE_VERSION) $(MIN_VERSION)))),$(error $(VERSION_ERROR))),)
+
+self    := $(lastword $(MAKEFILE_LIST))
+$(self) := $(basename $(self))
+$(self):;
+
+top: phony; @date
+
+################
+
+git.version.min := 2.23.0
+git.version.use := $(word 3, $(shell git version))
+git.version.seq := <(echo $(git.version.min) $(git.version.use) | xargs -n1)
+git.version.cmp != cmp -s <(sort -V $(git.version.seq)) $(git.version.seq) || date
+$(if $(git.version.cmp), $(error want git $(git.version.min), found git $(git.version.use)))
+
+################
 
 toplevel != git rev-parse --show-toplevel
 $(if $(toplevel),, $(error not in git context))
@@ -12,24 +40,27 @@ $(if $(toplevel),, $(error not in git context))
 found := $(toplevel)/.git/hooks/.found-dates
 saved := $(toplevel)/.saved-dates
 
-save: find $(saved)
+save: phony find $(saved)
+main: phony save
 
 find restore: vars := -v RS='\0' -v q='"'
 
 find: find := git ls-files -sz | grep -zve $(notdir $(saved)) | cut -zf2
 find: stat := xargs -0 stat -c '%Y %n' | tr '\n' '\0' | sort -zrn
 find: awk  := FNR == 1 { print "touch -d @" $$1 FS q "$(found)" q }
-find:
+find: phony
  @$(find) | $(stat) > $(found)
  awk $(vars) '$(awk)' $(found) | dash
+ tr '\0' '\n' < $(found) > $(found)-nozero
  cmp -s $(found) $(saved) || rm -f $(saved)
 
+$(found):;
 $(saved): $(found); @cp -p $< $@; git add $@; echo $(self): dates upgraded
 
-show: $(saved); @< $< xargs -0i echo {}
+show: $(saved) phony; @< $< xargs -0i echo {}
 
 restore: awk := { print "touch -d @" $$1 sprintf("", sub($$1 FS, "")) FS q $$0 q }
-restore:; @test -f $(saved) && awk $(vars) '$(awk)' $(saved) | dash
+restore: phony; @test -f $(saved) && awk $(vars) '$(awk)' $(saved) | dash
 
 hooks: list       := pre-commit post-merge
 hooks: pre-commit := \#!/bin/sh\n\ngit-store-dates
@@ -47,13 +78,7 @@ $(hooks_links_p): $(hooks_files_p); (cd $(@D); ln -s $(<F) $(@F))
 $(hooks_tests):
  @test -h $(basename $@) || (test -f $(basename $@) \
  && (echo -e "\n\t*** hook $(basename $(@F)) already exists\n"; exit 1))
-hooks: $(hooks_files) $(hooks_links) $(hooks_tests);
-
-.PHONY: find save show restore install hooks
-
-self    := $(lastword $(MAKEFILE_LIST))
-$(self) := $(basename $(self))
-$(self):;
+hooks: phony $(hooks_files) $(hooks_links) $(hooks_tests);
 
 ifeq ($(MAKECMDGOALS), install)
 
@@ -68,9 +93,9 @@ ifeq ($(dir $(self)),./)
 install_dir := /usr/local/bin
 install_list := $(self)
 $(install_dir)/%: %; install $< $@; $(if $($*),(cd $(@D); $(strip $(foreach _, $($*), ln -sf $* $_;))))
-install: $(install_list:%=$(install_dir)/%);
+install: phony $(install_list:%=$(install_dir)/%);
 else
-install:; @echo "can't install from installed"
+install: phony; @echo "can't install from installed"
 endif
 
 endif
